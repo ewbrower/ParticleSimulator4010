b1f91686fa6a1ccff1dd7972f17251a46bcb0d86
Eric W Brower <ewbrower@gmail.com>
2015-10-28 13:46:38 -0400

    generic returns that do nothing

--- Particle.c
+++ Particle.c
@@ -1,1 +1,32 @@
 #include "Particle.h"
+
+double GetX(struct Particle)
+{
+    return 0.0;
+}
+
+double GetY(struct Particle)
+{
+    return 0.0;
+}
+
+double GetZ(struct Particle)
+{
+    return 0.0;
+}
+
+
+double GetXSpeed(struct Particle)
+{
+    return 0.0;
+}
+
+double GetYSpeed(struct Particle)
+{
+    return 0.0;
+}
+
+double GetZSpeed(struct Particle)
+{
+    return 0.0;
+}
