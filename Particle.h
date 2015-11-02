2acc7cdaeed1f953166bdf8ed3d05cee72606e40
Eric W Brower <ewbrower@gmail.com>
2015-11-01 23:37:37 -0500

    updated the txt file to fix how we set particle position; added the docx and the pdf; updated the h files!

--- Particle.h
+++ Particle.h
@@ -1,14 +1,8 @@
 #ifndef PARTICLE_H
 #define PARTICLE_H
 
-// Eric will do all three of these particle functions
-// (because they are easy and he's choosing)
-//
 // get position returns an array of three double
 double* GetPosition(int partIndex);
-
-// set position
-int SetPosition(Simulation sim, int partIndex, int x, int y, int z);
 
 // Brownian returns an array of three doubles of Brownian displacement
 double* GetBrownian();
--- Simulation.h
+++ Simulation.h
@@ -5,26 +5,20 @@
 struct Simulation;
 typedef Simulation Simulation;
 
-// Eric will handle these functions
-//
 // create simulation
 Simulation* CreateSimulation(double l, int b);
 // generate the array of random Particles (private)
 int CreateParticles(Simulation sim);
 
-// Cameron will do the particle functions as per each iteration
-// 
 // update simulation to next timestep (which is known)
 void Update(Simulation sim);
 // particle distance getter thing
 double GetParticleDistance(Simulation sim, int partIndexA, int partIndexB);
 // find net force
 double* SimulationNetForce(Simulation sim, int partIndex);
-// check if in box
-double* SimulationInBox(Simulation sim, int partIndex);
+// check if in box update position to correct one
+double* SetPosition(Simulation sim, int partIndex, double x, double y, double z);
 
-// Abi will handle properly writing the output to xyz files
-//
 // write to the xyz file
 void WriteToFile(Simulation sim);
 
