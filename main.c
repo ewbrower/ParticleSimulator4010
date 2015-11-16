f36985ddc170e7d83e89d59f2a2b53a7bff7ab61
Cameron McMillan <ezmac12@gmail.com>
2015-11-15 22:43:17 -0500

    Clock Time Update

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
+#inlcude <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "Simulation.h"
+
 int main(int npos, double L, int nsteps)
 {
-
+clock_t start_time, end_time;
+double avg_time;
   int parts, i, j, k;
   //verify valuable parameters are entered
 	if (L <= 0 || npos <= 0 || nsteps <= 0)
@@ -9,29 +15,40 @@ int main(int npos, double L, int nsteps)
 		return -1;
 	}
 
-  Simulation *sim;
-  sim = CreateSimulation(L, npos);
+  
   printf("Created simulation!\nbox length: %f\nparticles: %d\n", L, npos);
 
-  parts = CreateParticles(sim);
-
-  for (i = 0; i < steps; i++)
+  
+  start_time = clock();
+  for(j = 0; j < 100; j++)
+  {
+	Simulation *sim;
+	sim = CreateSimulation(L, npos);
+	parts = CreateParticles(sim);
+	WriteToFile(sim);
+	for (i = 0; i < steps; i++)
 	{
-    WriteToFile(sim); //Calls WriteToFile function
-    SimulationNetForce(sim);
+		
+		//Calls WriteToFile function
 		Update(sim);
-		printf("-- Update %d Force Summary --\n", i);
-		for(j = 0; j < npos*3; j+=3)
-		{
-			printf("Particle %d\n", j/3);
-			for (k = 0; k < 3; k++)
-			{
-				printf("%f ", sim->force_arr[j+k]);
-			}
-			printf("\n");
-		}
-		printf("\n");
+		//printf("-- Update %d Force Summary --\n", i);
+		WriteToFile(sim);
+		//for(j = 0; j < npos*3; j+=3)
+		//{
+		//	printf("Particle %d\n", j/3);
+		//	for (k = 0; k < 3; k++)
+		//	{
+		//		printf("%f ", sim->force_arr[j+k]);
+		//	}
+		//	printf("\n");
+		//}
+		//printf("\n");
 	}
+	DestroySimulation(sim);
+  }
+	end_time = clock();
+	avg_time = ((end_time - start_time)*1000)/(CLOCKS_PER_SEC*100)
+	printf("Number of particle: %d\nTime elapsed: %f\n", npos, avg_time);
 
 	return 0;
 
